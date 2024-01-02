944cf3574307d8259d4b13c89053b52287b757e4
Youcef <117318791+Achouriyoucefislem@users.noreply.github.com>
2024-01-02 18:20:05 +0100

    ..

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import react from 'react'
+import reactdom from 'react-dom/client'
+
+reactdom.createRoot( document.getElementById('root') ).render( <h1>Hello world!</h1> )
--- test.js
+++
@@ -1,5 +0,0 @@
-var a = 5
-
-delete a
-
-console.log(a)
