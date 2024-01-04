2efb1514be94b7fcaffef8d62878e3f870b26eb1
Youcef <117318791+Achouriyoucefislem@users.noreply.github.com>
2024-01-04 10:46:27 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 
 export default () => {
 
-  useEffect( 
-    () => {console.log(window.innerWidth)} , [window.innerWidth]
-   )
+  useEffect( () => {
+    fetch( 'https://raw.githubusercontent.com/Achouriyoucefislem/first-project/main/src/data.js' )
+    .then( result => result.json() )
+    .then( result => console.log(result) ) }
+    )
 
   return <React.Fragment>
     <button>click me</button>
