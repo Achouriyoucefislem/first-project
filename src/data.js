ce608916e0d2804ce359f56ccdc3ff747a48b4a0
Youcef <117318791+Achouriyoucefislem@users.noreply.github.com>
2024-01-04 10:54:24 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,22 +1,7 @@
 [
   {
-    id : 1,
-    name : 'youcef',
-    age : 28
-  },
-  {
-    id : 2,
-    name : 'ilyes',
-    age : 38
-  },
-  {
-    id : 3,
-    name : 'achouri',
-    age : 28
-  },
-  {
-    id : 4,
-    name : 'nina',
-    age : 28
+    "id" : "1",
+    "name" : "youcef",
+    "age" : "28"
   }
 ]
